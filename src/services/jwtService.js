9c0e768b0940c03717993fcadb26b2e1f7e6d712
Thai Bao Duy <thaibaoduy.5724@gmail.com>
2024-09-21 23:31:53 +0700

    part7

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -8,7 +8,7 @@ const genneralAccessToken = async (payload) => {
       payload,
     },
     process.env.ACCESS_TOKEN,
-    { expiresIn: "1h" }
+    { expiresIn: "30s" }
   );
   return access_token;
 };
@@ -24,4 +24,40 @@ const genneralRefreshToken = async (payload) => {
   return refresh_token;
 };
 
-module.exports = { genneralAccessToken, genneralRefreshToken };
+const refreshTokenJwtService = async (token) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log("token", token);
+      jwt.verify(token, process.env.REFRESH_TOKEN, async (err, user) => {
+        if (err) {
+          resolve({
+            status: "ERROR",
+            message: "The authemtication",
+          });
+        }
+        console.log("user", user);
+
+        const { payload } = user;
+        const access_token = await genneralAccessToken({
+          id: payload?.id,
+          // email: payload.email,
+          isAdmin: payload?.isAdmin,
+        });
+
+        console.log("access token", access_token);
+        resolve({
+          status: "OK",
+          message: "Successfully",
+          access_token,
+        });
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+module.exports = {
+  genneralAccessToken,
+  genneralRefreshToken,
+  refreshTokenJwtService,
+};
