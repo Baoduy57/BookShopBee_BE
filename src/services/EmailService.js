1f4d0832c34926c43ec164371af7a6abe496b316
Thai Bao Duy <thaibaoduy.5724@gmail.com>
2024-10-23 01:29:56 +0700

    part26

---
+++ src/services/EmailService.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sendEmailCreateOrder = async (email, orderItems) => {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for port 465, false for other ports
+    auth: {
+      user: process.env.MAIL_ACCOUNT,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
+
+  let listItem = "";
+  const attachImage = [];
+  orderItems.forEach((order) => {
+    listItem += `<div>
+<div>Bạn đã đặt sản phẩm <b>${order.name}</b> với số lượng: <b>${order.amount}</b> và giá là: <b>${order.price} VND</b></div>
+
+</div>`;
+    attachImage.push({ path: order.image });
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: process.env.MAIL_ACCOUNT, // địa chỉ người gửi
+    to: process.env.MAIL_ACCOUNT, // danh sách người nhận
+    subject: "Hello bạn đã đặt hàng tại BookShopBee ✔", //Tiêu đề email
+    text: "Bạn đã đặt hàng thành công tại BookShopBee", // nội dung dạng văn bản
+    html: `<div><b>Bạn đã đặt hàng thành công tại BookShopBee</b></div>${listItem}`, // html body
+    attachments: attachImage, // file đính kèm
+  });
+};
+
+module.exports = { sendEmailCreateOrder };
